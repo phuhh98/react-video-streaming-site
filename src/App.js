e5755dd762b7a60bf587c42f2286af437c934251
Phu Huynh <phu_huynh@epam.com>
2022-02-15 19:15:47 +0700

    Basic Routing to home, about and contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.png';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 
-function App(props) {
-	return <div className="App">{props.children}</div>;
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Homepage from './components/pages/Homepage';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import NotFound from './components/pages/NotFound';
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Main>
+				<Routes>
+					<Route path="/home" element={<Homepage />} />
+					<Route path="/" element={<Homepage />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Main>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
