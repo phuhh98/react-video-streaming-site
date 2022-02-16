7c611fa7f50ef99827e8b303b468e5e5063bd934
Phu Huynh <phu_huynh@epam.com>
2022-02-16 11:20:24 +0700

    Move ImageContainer to new folder

--- src/components/imageContainer/ImageContainer.jsx
+++ src/components/imageContainer/ImageContainer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default function ImageContainer(props) {
 	return (
 		<div style={{ width: props.width }}>
-			<img src={props.imageSource} style={{ width: '100%' }} />
+			<img
+				src={props.src}
+				style={{ width: '100%' }}
+				alt={props.alt ? props.alt : ''}
+			/>
 		</div>
 	);
 }
