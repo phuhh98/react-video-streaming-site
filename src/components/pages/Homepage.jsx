752425935afeee0a6a2475dd89a8f2c27aa0f335
Phu Huynh <phu_huynh@epam.com>
2022-02-16 14:06:20 +0700

    Using context, state for like and fav, split film items view to another file

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,43 +1,61 @@
-import React from 'react';
-import { CardGroup, Row, Col } from 'reactstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+	Container,
+	Row,
+	Col,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
 
-import FilmCard from '../film-card/FilmCard';
-export default function HomePage() {
+import AppContext from '../contexts/AppContext';
+import FilmList from '../filmList/FilmList';
+import StyledLink from '../utilWrapper/StyledLink';
+export default React.memo(function HomePage() {
+	const [filmData, setFilmData] = useContext(AppContext);
+	const [pageData, setPageData] = useState([]);
+	useEffect(() => {
+		console.log('FilmData in context printed from homepage \n', filmData);
+		fetch('https://api.tvmaze.com/shows')
+			.then(response => response.json())
+			.then(data => {
+				setFilmData(data);
+				setPageData(data.slice(0, 99));
+			});
+	}, []);
 	return (
 		<>
-			<p>The Home page</p>
-			<Row lg="4" md="3" sm="2" xs="1">
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>{' '}
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>{' '}
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>{' '}
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>{' '}
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>{' '}
-				<Col className="bg-light">
-					<FilmCard></FilmCard>
-				</Col>
-			</Row>
+			<Container>
+				<UncontrolledDropdown>
+					<DropdownToggle caret color="secondary">
+						Genre
+					</DropdownToggle>
+					<DropdownMenu container="body">
+						<StyledLink to="/search?q=comedy">
+							<DropdownItem onClick={function noRefCheck() {}}>
+								Comedy
+							</DropdownItem>
+						</StyledLink>
+						<StyledLink to="/search?q=action">
+							<DropdownItem onClick={function noRefCheck() {}}>
+								Action
+							</DropdownItem>
+						</StyledLink>
+						<StyledLink to="/search?q=thriller">
+							<DropdownItem onClick={function noRefCheck() {}}>
+								Thriller
+							</DropdownItem>
+						</StyledLink>
+						<StyledLink to="/search?q=horror">
+							<DropdownItem onClick={function noRefCheck() {}}>
+								Horror
+							</DropdownItem>
+						</StyledLink>
+					</DropdownMenu>
+				</UncontrolledDropdown>
+				<FilmList filmList={pageData} />
+			</Container>
 		</>
 	);
-}
+});
