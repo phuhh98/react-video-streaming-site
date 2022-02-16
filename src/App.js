f0c191b5d266cbe5a6ef0c6d7a416c006df70399
Phu Huynh <phu_huynh@epam.com>
2022-02-16 14:01:14 +0700

    Create app context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import AppRouter from './components/routers/AppRouter';
+import AppContext from './components/contexts/AppContext';
 
 function App() {
+	const [filmData, setFilmData] = useState({});
 	return (
 		<div className="App">
-			<AppRouter />
+			<AppContext.Provider value={[filmData, setFilmData]}>
+				<AppRouter />
+			</AppContext.Provider>
 		</div>
 	);
 }
---
+++ src/components/contexts/AppContext.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const AppContext = React.createContext(null); // create context to share data
+export default AppContext;
