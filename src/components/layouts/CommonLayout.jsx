227392e6e0168bf71647249b5a868490ef6d395d
Phu Huynh <phu_huynh@epam.com>
2022-02-16 11:19:21 +0700

    Move layout components to new folder

---
+++ src/components/layouts/CommonLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
+
+export default function CommonLayout(props) {
+	return (
+		<>
+			<Header />
+			<Main>
+				<Outlet />
+			</Main>
+			<Footer />
+		</>
+	);
+}
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem } from 'reactstrap';
 
-import ImageContainer from './ImageContainer.jsx';
-import logo from '../logo.png';
+import ImageContainer from '../imageContainer/ImageContainer';
+import logo from '../../logo.png';
 
 export default function Header(props) {
 	return (
@@ -14,38 +14,35 @@ export default function Header(props) {
 				gridTemplateColumns: '120px auto',
 				gridTemplateRows: 'auto auto',
 				backgroundColor: '#393939',
-				boxShadow: '0 3px 3px 3px #d3d3d3',
+				boxShadow: '0 3px 5px 5px #d3d3d3',
 				margin: '0 auto',
 				color: '#fff'
 			}}
 		>
 			<Link to="/home" style={{ gridColumn: '1/2', gridRow: '1/3' }}>
 				{' '}
-				<ImageContainer imageSource={logo} width="120px"></ImageContainer>
+				<ImageContainer src={logo} width="120px"></ImageContainer>
 			</Link>
 			<Nav
-				pill
+				pills
 				style={{
 					justifyItems: 'center',
 					alignItems: 'center',
 					margin: '0 auto',
-					paddingRight: '10%'
+					paddingRight: '10%',
+					display: 'flex',
+					justifyContent: 'space-between',
+					width: '50%'
 				}}
 			>
 				<NavItem>
-					<Link to="/home">
-						<NavLink>Home page</NavLink>
-					</Link>
+					<Link to="/home">Home page</Link>
 				</NavItem>
 				<NavItem>
-					<Link to="/about">
-						<NavLink>About us</NavLink>
-					</Link>
+					<Link to="/about">About us</Link>
 				</NavItem>
 				<NavItem>
-					<Link to="/contact">
-						<NavLink>Contact</NavLink>
-					</Link>
+					<Link to="/contact">Contact</Link>
 				</NavItem>
 			</Nav>
 			<h2
