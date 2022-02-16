7e347211ad7d10c0a6f63a346d21edb2d8d7f6f6
Phu Huynh <phu_huynh@epam.com>
2022-02-16 11:21:12 +0700

    Edit Homepage for researching for solution

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import { CardGroup, Row, Col } from 'reactstrap';
 
-export default function () {
-	return <p>The Home page</p>;
+import FilmCard from '../film-card/FilmCard';
+export default function HomePage() {
+	return (
+		<>
+			<p>The Home page</p>
+			<Row lg="4" md="3" sm="2" xs="1">
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>{' '}
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>{' '}
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>{' '}
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>{' '}
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>{' '}
+				<Col className="bg-light">
+					<FilmCard></FilmCard>
+				</Col>
+			</Row>
+		</>
+	);
 }
