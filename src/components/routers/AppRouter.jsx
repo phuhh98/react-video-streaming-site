aeb78238f2e98dba60772971dc716623d3d77137
Phu Huynh <phu_huynh@epam.com>
2022-02-17 13:50:32 +0700

    Add new route to :genre/:pageNumber

--- src/components/routers/AppRouter.jsx
+++ src/components/routers/AppRouter.jsx
@@ -5,17 +5,23 @@ import Homepage from '../pages/Homepage';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import NotFound from '../pages/NotFound';
+import Genre from '../pages/Genre';
 
 export default function AppRouter() {
 	return (
 		<Routes>
 			<Route path="/" element={<CommonLayout />}>
 				<Route index element={<Homepage />} />
-				<Route path="home/" element={<Homepage />}>
+				<Route path="home" element={<Homepage />}>
 					<Route path=":pageNumber" element={<Homepage />} />
 				</Route>
 				<Route path="about" element={<About />} />
 				<Route path="contact" element={<Contact />} />
+				<Route path="genre" element={<Genre />}>
+					<Route path=":genre" element={<Genre />}>
+						<Route path=":pageNumber" element={<Genre />}></Route>
+					</Route>
+				</Route>
 				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
