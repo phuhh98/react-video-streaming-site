ab570b8f04d34ab2015a5bf062142c77a1dfd63a
Phu Huynh <phu_huynh@epam.com>
2022-02-17 18:38:45 +0700

    Conditional switch to original when medium image not found

--- src/components/commons/filmList/FilmList.jsx
+++ src/components/commons/filmList/FilmList.jsx
@@ -9,14 +9,13 @@ export default function FilmList(props) {
 		<Row lg="4" md="3" sm="2" xs="1">
 			{props.filmList.map(film => {
 				const filmData = {
-					imgSrc: film.image.medium,
+					imgSrc: !!film.image.medium ? film.image.medium : film.image.original,
 					imgAlt: film.name,
 					genres: film.genres,
 					filmTitle: film.name,
 					rating: film.rating.average,
 					id: film.id
 				};
-				// console.log(filmData);
 				return (
 					<Col className="bg-light" key={film.id}>
 						<FilmCard data={filmData} />
