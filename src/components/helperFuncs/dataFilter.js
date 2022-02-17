c0430bda0cf3df7c09e7c923e3d1fd2338748331
Phu Huynh <phu_huynh@epam.com>
2022-02-17 17:09:44 +0700

    Change to genreFilter

--- src/components/helperFuncs/dataFilter.js
+++ src/components/helperFuncs/dataFilter.js
@@ -1,6 +1,5 @@
 import firstLetterUpperCase from './firstLetterUpperCase';
 
-export function GenreFilter(data, genre) {
-	console.log('filter', data, genre.toLowerCase);
+export function genreFilter(data, genre) {
 	return data.filter(item => item.genres.includes(firstLetterUpperCase(genre)));
 }
