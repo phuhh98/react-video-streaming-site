f16471d86e38d72b84d4dca912500f01aaf60efa
Phu Huynh <phu_huynh@epam.com>
2022-02-18 09:14:53 +0700

    Add constraint to check availability of genre

--- src/components/helperFuncs/dataFilter.js
+++ src/components/helperFuncs/dataFilter.js
@@ -1,5 +1,9 @@
 import firstLetterUpperCase from './firstLetterUpperCase';
-
-export function genreFilter(data, genre) {
-	return data.filter(item => item.genres.includes(firstLetterUpperCase(genre)));
+export function genreFilter(data, { genre }) {
+	if (!!genre) {
+		return data.filter(item =>
+			item.genres.includes(firstLetterUpperCase(genre))
+		);
+	}
+	return data;
 }
