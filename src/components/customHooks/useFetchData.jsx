0d47b666ab11355ef4af57fdf77500947a8a4cf7
Phu Huynh <phu_huynh@epam.com>
2022-02-18 11:30:54 +0700

    Filter filtedFilms to remove duplicated item compare to last api call - what a weird api

--- src/components/customHooks/useFetchData.jsx
+++ src/components/customHooks/useFetchData.jsx
@@ -13,11 +13,11 @@ export function useFetchFilmData(
 ) {
 	//api paging
 	const [queryPage, setQueryPage] = useState(0);
-
 	const [reupdate, setReupdate] = useState(false);
-
+	const [oldGenre, setOldGenre] = useState(params.genre);
 	//item per view page
 	const ItemPerPage = 20;
+
 	useEffect(() => {
 		//current api query page
 		let tempQueryPage = queryPage;
@@ -25,12 +25,13 @@ export function useFetchFilmData(
 
 		tempFilmData = filter(tempFilmData, params);
 
-		//Reset queryPage on page 0
-		if (parseInt(params.pageNumber) === 0 && queryPage !== 0) {
-			setQueryPage(0);
-			setReupdate(!reupdate);
-			return;
+		//cleart tempFilmData when genre changed
+		if (params.genre !== oldGenre) {
+			tempFilmData = [];
+			setOldGenre(params.genre);
 		}
+
+		console.log('queryPage: ', queryPage);
 
 		//if current item list is enough => trigger render
 		if ((pageNumber + 1) * ItemPerPage <= tempFilmData.length && !!pageNumber) {
@@ -39,36 +40,44 @@ export function useFetchFilmData(
 			return;
 		}
 
-		//if current items list is not enough => increase queryPage on api
-		if (!!tempFilmData.length) {
-			if ((pageNumber + 1) * ItemPerPage >= tempFilmData.length) {
-				tempQueryPage++;
-				setQueryPage(tempQueryPage);
-			}
-		}
-
 		fetch(`https://api.tvmaze.com/shows?page=${tempQueryPage}`)
 			.then(response => response.json())
 			.then(data => {
-				let filteredFilms = filter(data, params.genre);
+				let filteredFilms = filter(data, params);
 
 				//on zero page => reset tempFilmData
-
-				if (!params.pageNumber || parseInt(params.pageNumber) === 0) {
+				if (
+					(!params.pageNumber || parseInt(params.pageNumber) === 0) &&
+					queryPage === 0
+				) {
 					console.log('reset temp');
 					tempFilmData = [];
 					tempFilmData.push(...filteredFilms);
 				} else {
+					//Filter duplicated item on api call -- oh my god!! what an API
+					filteredFilms = filteredFilms.reduce((acc, filteredItem) => {
+						if (
+							tempFilmData.find(item => {
+								if (item.id === filteredItem.id) {
+									return true;
+								}
+							})
+						) {
+							return acc;
+						}
+						acc.push(filteredItem);
+						return acc;
+					}, []);
+					console.log(filteredFilms);
 					tempFilmData.push(...filteredFilms);
 				}
 				setFilmData(tempFilmData);
-				console.log(tempFilmData);
 
 				if ((pageNumber + 1) * ItemPerPage >= tempFilmData.length) {
 					setReupdate(!reupdate);
+					return;
 				}
 
-				console.log('fetched');
 				const startItemIndex = 0 + ItemPerPage * pageNumber;
 				const displayItems = tempFilmData.slice(
 					startItemIndex,
@@ -76,14 +85,27 @@ export function useFetchFilmData(
 				);
 				setPageData(displayItems);
 			});
+		//if current items list is not enough => increase queryPage on api
+		if (!!tempFilmData.length) {
+			if ((pageNumber + 1) * ItemPerPage >= tempFilmData.length) {
+				tempQueryPage++;
+				setQueryPage(tempQueryPage);
+			}
+		}
 	}, [pageNumber, params.genre, params.pageNumber, reupdate]);
 
+	//reset page to zero on navigate to home
 	useEffect(() => {
-		console.log(pathname);
 		if (pathname === '/home') {
 			setPageNumber(0);
 		}
 	}, [pathname]);
+
+	//reset queryPage and pageNumber when change genre filter
+	useEffect(() => {
+		setQueryPage(0);
+		setPageNumber(0);
+	}, [params.genre]);
 }
 
 function noFilter(arr) {
