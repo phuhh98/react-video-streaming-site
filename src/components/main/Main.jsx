621bf1828292cb99c4f7ef2272703ffcdbbbc4c1
Phu Huynh <phu_huynh@epam.com>
2022-02-21 15:30:04 +0700

    Update Main component as styled component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Main(props) {
-	const styleOption = {
-		margin: '0 auto',
-		maxWidth: '80%'
-	};
-	return <main style={styleOption}>{props.children}</main>;
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+  margin: 0 auto;
+  maxwidth: '80%';
+`;
+
+export default function (props) {
+  return <MainContainer {...props}></MainContainer>;
 }
