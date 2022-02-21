85830a24fee5f577f7c34fa2cd6aa8d16ca9825a
Phu Huynh <phu_huynh@epam.com>
2022-02-21 14:43:37 +0700

    Remove 'fiction' in genreList array

--- src/App.js
+++ src/App.js
@@ -3,34 +3,33 @@ import AppRouter from './components/routers/AppRouter';
 import AppContext from './components/contexts/AppContext';
 
 function App() {
-	const [filmData, setFilmData] = useState([]);
-	const app = {
-		filmData,
-		setFilmData,
-		login: false,
-		likedShowIds: [],
-		favoriteShowIds: [],
-		genreList: [
-			'adventure',
-			'comedy',
-			'action',
-			'horror',
-			'thriller',
-			'science-fiction',
-			'crime',
-			'fiction',
-			'drama',
-			'mystery',
-			'anime'
-		].sort((a, b) => (a > b ? 1 : -1))
-	};
-	return (
-		<div className="App">
-			<AppContext.Provider value={app}>
-				<AppRouter />
-			</AppContext.Provider>
-		</div>
-	);
+  const [filmData, setFilmData] = useState([]);
+  const app = {
+    filmData,
+    setFilmData,
+    login: false,
+    likedShowIds: [],
+    favoriteShowIds: [],
+    genreList: [
+      'adventure',
+      'comedy',
+      'action',
+      'horror',
+      'thriller',
+      'science-fiction',
+      'crime',
+      'drama',
+      'mystery',
+      'anime',
+    ].sort((a, b) => (a > b ? 1 : -1)),
+  };
+  return (
+    <div className="App">
+      <AppContext.Provider value={app}>
+        <AppRouter />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
