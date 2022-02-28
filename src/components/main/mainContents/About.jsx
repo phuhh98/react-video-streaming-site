d41d96cd734e0e7d8f4ae30ae44e4779e8524552
Phu Huynh <phu_huynh@epam.com>
2022-02-28 13:58:34 +0700

    Edit About component to show some details about the project

--- src/components/main/mainContents/About.jsx
+++ src/components/main/mainContents/About.jsx
@@ -1,4 +1,36 @@
-import React from 'react';
-export default function () {
-  return <p>A learning project from Phu Huynh, R&amp;D team Vietnam</p>;
+import React from "react";
+import { MainContent } from "./styledComponents/ContainerStyled";
+export default function About() {
+  return (
+    <>
+      <MainContent>
+        <br />
+        <h2>About the project</h2>
+        <br />
+        <h3>What is it?</h3>
+        <p>R&amp;D project to learn React - Front-end JavaScript library</p>
+        <br />
+        <h3>Project expectations</h3>
+        <p>
+          <ul>
+            <li>User can browse movies</li>
+            <li>Using filter to look for movies with the same genre</li>
+            <li>
+              Can create personal account and put movie to like and favorite
+              categories
+            </li>
+          </ul>
+        </p>
+        <br />
+        <h3>Author</h3>
+        <p>Hoai Phu Huynh, EPAM System Vietnam</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </MainContent>
+    </>
+  );
 }
