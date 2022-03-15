418b26de2d37ce7232ee54adf9299dc78d14dc88
Phu Huynh <phu_huynh@epam.com>
2022-03-15 10:35:51 +0700

    Rename styledContainer to FilmCardStyles, move Enlarge to utilWrapper

---
+++ src/components/filmCard/styledComponent/FilmCardStyles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import {
+  Container,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardImg,
+  Button,
+} from 'reactstrap';
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
